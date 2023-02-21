bfbe0422f53f9dfa8fbfa1cc1c10f8c7c780e693
Dominik Slahor <slahordominik@gmail.com>
2023-02-21 18:55:07 +0100

    BinarySearchTree - BST

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+    BinarySearch tree = new BinarySearch();
+
+        tree.insert(new Node(5));
+        tree.insert(new Node(1));
+        tree.insert(new Node(9));
+        tree.insert(new Node(2));
+        tree.insert(new Node(7));
+        tree.insert(new Node(3));
+        tree.remove(2);
+        tree.search(7);
+
+    tree.display();
+    }
+}
